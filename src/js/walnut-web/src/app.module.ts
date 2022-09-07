eed05ad55ddcd878e205f9ca118fb3c241b08561
kruemelmann <kruemelmannlp@gmail.com>
2022-09-07 16:08:50 +0200

    adding an angular based gui

--- src/js/walnut-web/src/app.module.ts
+++ src/js/walnut-web/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import {ServeStaticModule} from '@nestjs/serve-static';
+import {join} from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+      ServeStaticModule.forRoot({
+//TODO think about a better way to configure a path to the frontend files. Maybe a environment variable so localy i can use direnv and inside a container a can use dockers environment variable.
+          rootPath: join(__dirname, '..', 'client'),
+      }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
