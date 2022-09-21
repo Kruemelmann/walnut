fd784ae066f5174a260cb619d82396329655cb90
kruemelmann <kruemelmannlp@gmail.com>
2022-09-21 15:49:12 +0200

    implementing the grpc server for the auth service

---
+++ src/auth/authserver.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	walnut "github.com/kruemelmann/walnut/src/auth/genproto"
+)
+
+type authServer struct {
+	walnut.UnimplementedAuthServiceServer
+}
+
+func (a *authServer) ValidateToken(ctx context.Context, in *walnut.ValidateTokenRequest) (*walnut.ValidateTokenResponse, error) {
+
+	fmt.Printf("Validate Token %s\n", in.GetToken())
+
+	//TODO just for testing
+	isValid := false
+	if in.GetToken() == "123" {
+		isValid = true
+	}
+
+	return &walnut.ValidateTokenResponse{
+		//TODO validate with the db
+		IsValid: isValid,
+		User:    &walnut.User{},
+	}, nil
+}
