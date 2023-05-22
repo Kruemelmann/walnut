c59e75cedd80728587c02e46afa4c29ba01afc94
kruemelmann <kruemelmannlp@gmail.com>
2023-05-22 05:05:57 +0200

    adding docker build + env reading in web

--- src/web/go.mod
+++ src/web/go.mod
@@ -3,16 +3,29 @@ module github.com/kruemelmann/walnut/src/web
 go 1.18
 
 require (
-	github.com/google/uuid v1.1.2
+	github.com/google/uuid v1.3.0
 	github.com/gorilla/mux v1.8.0
-	google.golang.org/grpc v1.49.0
+	github.com/spf13/viper v1.15.0
+	google.golang.org/grpc v1.52.0
 	google.golang.org/protobuf v1.28.1
 )
 
 require (
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
-	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
-	golang.org/x/text v0.3.3 // indirect
-	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
+	github.com/spf13/afero v1.9.3 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.4.2 // indirect
+	golang.org/x/net v0.4.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+	google.golang.org/genproto v0.0.0-20221227171554-f9683d7f8bef // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- src/web/main.go
+++ src/web/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
 )
 
 var (
+	//web server settings
 	host = "localhost"
 	port = "8000"
-
+	// cookie settings
 	cookieSessionID     = "walnut_session-id"
 	cookieSessionMaxAge = 60 * 60 * 10 // 10 hours
 )
@@ -18,13 +23,37 @@ var (
 type ctxSessionID struct{}
 
 //TODO add the grpc bindings
-type webServer struct{}
+type webServer struct {
+	authAddr string
+	authCon  *grpc.ClientConn
+
+	farmerAddr string
+	farmerCon  *grpc.ClientConn
+
+	storeAddr string
+	storeCon  *grpc.ClientConn
+
+	dataAddr string
+	dataCon  *grpc.ClientConn
+}
 
 func main() {
-	//ctx := context.Background()
+	ctx := context.Background()
 
 	ws := new(webServer)
+	//read envs
+	extractEnv(&ws.authAddr, "AUTH_ADDRESS")
+	extractEnv(&ws.farmerAddr, "FARM_ADDRESS")
+	extractEnv(&ws.storeAddr, "STORE_ADDRESS")
+	extractEnv(&ws.dataAddr, "DATA_ADDRESS")
 
+	//connect grpc services
+	connectGRPCService(ctx, &ws.authCon, ws.authAddr)
+	connectGRPCService(ctx, &ws.farmerCon, ws.farmerAddr)
+	connectGRPCService(ctx, &ws.storeCon, ws.storeAddr)
+	connectGRPCService(ctx, &ws.dataCon, ws.dataAddr)
+
+	//configure webserver
 	r := mux.NewRouter()
 	r.HandleFunc("/", ws.testHandler).Methods(http.MethodGet, http.MethodHead)
 
@@ -34,4 +63,13 @@ func main() {
 	adress := host + ":" + port
 	log.Println("Server started on " + adress)
 	log.Fatal(http.ListenAndServe(adress, handler))
+}
+
+func connectGRPCService(ctx context.Context, con **grpc.ClientConn, addr string) {
+	fmt.Println(addr)
+}
+
+func extractEnv(dest *string, key string) {
+	viper.BindEnv(key)
+	*dest = fmt.Sprintf("%s", viper.Get(key))
 }
